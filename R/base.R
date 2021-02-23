d8f4b7a66e7d821b634c32f408314f0bbe9f7ddf
gerriosantos <gerriosantos@gmail.com>
2021-02-23 17:25:15 -0300

    usar ribge package para populacao

--- R/base.R
+++ R/base.R
@@ -5,29 +5,32 @@ pacman::p_load('tidyverse', 'geobr', 'sf')
 temp <- tempfile()
 url = 'http://download-integrasus.saude.ce.gov.br/download'
 download.file(url, temp, quiet = TRUE, mode = "wb")
-data <- read_csv2(unz(temp, "casos_coronavirus_2021_02_20.csv"))
+data <- read_csv2(unz(temp, "casos_coronavirus_2020_12_17.csv"))
 
 
 ### Serie diária do covid19 CE
 dataConf <- data %>% #filter(codigoMunicipioCaso == 230400) %>%
+  #distinct(identificadorCaso, .keep_all = T) %>%
   select(identificadorCaso, codigoMunicipioCaso, municipioCaso, bairroCaso, estadoCaso,
          idadeCaso, sexoCaso, faixaEtaria, dataNascimento, dataInicioSintomas,
          dataColetaExame, dataResultadoExame, resultadoFinalExame) %>%
-  mutate(dataInicioSintomas = ifelse(dataInicioSintomas == 'NaT', NA, dataInicioSintomas),
-         data1 = as.Date(coalesce(dataInicioSintomas, dataColetaExame, dataResultadoExame)),
+  mutate(#dataInicioSintomas = ifelse(dataInicioSintomas == 'NaT', NA, dataInicioSintomas),
+         data1 = (coalesce(dataResultadoExame, dataInicioSintomas, dataColetaExame)),
          estadoCaso = if_else(is.na(estadoCaso), 'CE', estadoCaso)) %>%
   filter(estadoCaso == 'CE' & resultadoFinalExame == 'Positivo') %>%
-  distinct(identificadorCaso, .keep_all = T) %>% drop_na(data1) %>%
+  distinct(identificadorCaso, .keep_all = T) %>% #drop_na(data1) %>%
   #filter(data1 > '2020-10-31') %>%
-  group_by(data1) %>% summarise(conf = n()) %>% mutate(confCum = cumsum(conf))
+  group_by(data1) %>% summarise(conf = n(), .groups = 'drop') %>%
+  mutate(confCum = cumsum(conf))
 
 
 
-#dataConf %>% #filter(data1 > '2020-10-31') %>%
-ggplot(dataConf, aes(x = data1, y = conf))+
+dataConf %>% #filter(data1 > '2020-10-31') %>%
+  drop_na(data1) %>%
+  ggplot(., aes(x = data1, y = conf))+
   geom_line(color = 'purple')+
-  scale_x_date(breaks = "30 days", date_labels = "%d%b%Y")+
-  scale_y_continuous(breaks = seq(0, max(dataConf$conf), by = 300))+
+  scale_x_date(breaks = "20 days", date_labels = "%d%b%Y")+
+  #scale_y_continuous(breaks = seq(0, max(dataConf$conf), by = 10000))+
   theme_bw()
 
 
@@ -36,7 +39,7 @@ mun_covid <- data %>%
   select(identificadorCaso, codigoMunicipioCaso, municipioCaso, bairroCaso, estadoCaso,
          idadeCaso, sexoCaso, faixaEtaria, dataNascimento, dataInicioSintomas,
          dataColetaExame, dataResultadoExame, resultadoFinalExame) %>%
-  mutate(dataInicioSintomas = ifelse(dataInicioSintomas == 'NaT', NA, dataInicioSintomas),
+  mutate(#dataInicioSintomas = ifelse(dataInicioSintomas == 'NaT', NA, dataInicioSintomas),
          data1 = as.Date(coalesce(dataInicioSintomas, dataColetaExame, dataResultadoExame)),
          estadoCaso = if_else(is.na(estadoCaso), 'CE', estadoCaso)) %>%
   filter(estadoCaso == 'CE' & resultadoFinalExame == 'Positivo') %>%
@@ -56,6 +59,8 @@ colnames(pop) <- c('mun', 'cod_mun', 'gentilico', 'prefeito', 'area', 'pop20')
 pop <- pop %>% mutate(cod_mun1 = as.numeric(cod_mun),
                       cod_mun1 = as.numeric(substr(cod_mun1,1,6))) %>%
   select(cod_mun1, area, pop20)
+
+
 
 
 
@@ -102,4 +107,30 @@ for(i in lista) {
 
   assign(paste0('g', i), g)
 }
+
+
+
+
+
+
+a = list(seq(as.Date('2020-01-01'), as.Date('2020-01-03'), 'day'), NA , NA, as.Date('2020-01-07'))
+b = list(NA, seq(from=as.Date('2020-01-02'), to=as.Date('2020-01-04'), by='day'), NA, NA)
+c = list(NA, NA, NA, as.Date('2020-01-04'), as.Date('2020-01-05'), NA)
+
+
+
+
+## testando a função coalesce do dplyr:
+base <- data.frame(d1 = c(seq(as.Date('2020-01-01'), as.Date('2020-01-07'), 'day')),
+                   d2 = c(seq(from=as.Date('2020-01-08'), to=as.Date('2020-01-14'), by='day')),
+                   d3 = c(seq(from=as.Date('2020-01-15'), to=as.Date('2020-01-21'), by='day')))
+
+base[1,1] <- NA
+base[2,3] <- NA
+base[4,1:2] <- NA
+base[5,2] <- NA
+base[6,2:3] <- NA
+base[7,c(1,3)] <- NA
+
+base %>% mutate(data=coalesce(d1,d2,d3))
 
